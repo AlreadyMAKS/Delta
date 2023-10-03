8d7fd1fdfd72b0247915961350af1ff0e9e66240
AlreadyMAKS <97000211+AlreadyMAKS@users.noreply.github.com>
2023-10-03 20:46:33 +0200

    Create ESP.lua

---
+++ ESP.lua
@@ -0,0 +1,49 @@
+local RunService = game:GetService("RunService")
+
+local function Draw(character)
+
+    local Highlight = character:FindFirstChild("Highlight")
+
+    if Highlight == nil then
+        Highlight = Instance.new("Highlight")
+    end
+
+    Highlight.OutlineTransparency = 1
+    Highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    Highlight.Name = "AYO"
+    Highlight.Parent = character
+
+    Highlight.Enabled = _G.ESPChamsActive
+    Highlight.FillColor = _G.ESPChamsColor
+    Highlight.FillTransparency = _G.ESPChamsTransparency
+
+    local Connection;
+	Connection = RunService.RenderStepped:Connect(function()
+        Highlight.Enabled = _G.ESPChamsActive
+        Highlight.FillColor = _G.ESPChamsColor
+        Highlight.FillTransparency = _G.ESPChamsTransparency
+
+        if Highlight == nil or Highlight.Parent == nil then
+            Connection:Disconnect()
+        end
+	end)
+
+end
+
+for _, v in pairs(game.Players:GetPlayers()) do
+    if v.Character then
+        Draw(v.Character)
+    end
+    v.CharacterAdded:Connect(function(character)
+        Draw(character)
+    end)
+end
+
+game.Players.PlayerAdded:Connect(function(v)
+    if v.Character then
+        Draw(v.Character)
+    end
+    v.CharacterAdded:Connect(function(character)
+        Draw(character)
+    end)
+end)
